e7b46f7e79e3c3d9f4bce92bd21a16874e664219
Yuliia <innishastudy@gmail.com>
2026-02-20 17:23:02 +0200

    solved the task

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,31 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(
+                () -> cache.computeIfAbsent(
+                            userId,
+                            id -> {
+                                try {
+                                    Thread.sleep(200);
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                return new UserData(id, "info about user with id: " + id);
+                            }
+                    ), executor
+        );
     }
 }
